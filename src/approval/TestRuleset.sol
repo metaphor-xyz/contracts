8b319024e9545a1ade4cbfad056261882f6b3a6d
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-01-31 11:21:30 -0500

    refactor into CommitteeManager (#2)

--- src/approval/TestRuleset.sol
+++ src/approval/TestRuleset.sol
@@ -2,49 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IRuleset.sol";
-import "./Approval.sol";
+import "./CommitteeManager.sol";
 
-contract TestRuleset is IRuleset, Ownable {
-    string public name = "TestRuleset";
-    string public description = "This is a test ruleset";
-    Approval public approvalContract;
+contract TestRuleset is Ownable {
+    CommitteeManager public manager;
 
-    constructor(Approval approval) Ownable() {
-        approvalContract = approval;
-    }
-
-    function getMembers() external view returns (address[] memory members) {
-        address[] memory empty = new address[](0);
-
-        return empty;
-    }
-
-    function getName() external view returns (string memory) {
-        return name;
-    }
-
-    function getDescription() external view returns (string memory) {
-        return description;
+    constructor(CommitteeManager _manager) Ownable() {
+        manager = _manager;
     }
 
     function approve(uint256 committeeId, uint256 approvalRequestId)
         external
         onlyOwner
     {
-        require(
-            address(approvalContract) != address(0),
-            "Approval contract not set"
-        );
+        require(address(manager) != address(0), "Approval contract not set");
 
-        approvalContract.changeStatus(
+        manager.changeStatus(
             committeeId,
             approvalRequestId,
             ApprovalStatus.Approved
         );
     }
 
-    function setApprovalContract(address approvalAddress) external onlyOwner {
-        approvalContract = Approval(approvalAddress);
+    function setApprovalContract(address managerAddress) external onlyOwner {
+        manager = CommitteeManager(managerAddress);
     }
 }
